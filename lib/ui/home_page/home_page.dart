57dbb9dd1e134e494ed58442f4aec40499e02fdb
ludovic <ludo.spysschaert@orange.fr>
2024-03-05 16:05:30 +0100

    mise en forme des boutons route

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:mon_site_cv/ui/common/theme/theme.dart';
 
+import '../common/bouton-route/bouton-route.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.fromLTRB(30, 30, 0, 0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text('spysschaert', style: titleStyleMedium(context),)
-        ],
-      ),
-    );
+    if ( MediaQuery.sizeOf(context).width < 650) {
+      return Center(
+        child:
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('spysschaert', style: titleStyleMedium(context),
+              ),
+              Text('Ludovic', style: titleStyleMedium(context),
+              ),
+              Text('Développeur web', style: titleStyleMedium(context),
+              ),
+              Text('& web mobile', style: titleStyleMedium(context),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ButtonRoute(),
+                  ButtonRoute(),
+                  ButtonRoute(),
+                ],
+              )
+
+            ],
+          ),
+        ),
+      );
+    }else {
+      return
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('spysschaert', style: titleStyleMedium(context),
+              ),
+              Text('Ludovic', style: titleStyleMedium(context),)
+            ],
+          ),
+      );
+
+    }
   }
 }
